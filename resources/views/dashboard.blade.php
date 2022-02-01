ba5397bacb85bb20fb99548883cc5d5dffc814ee
Dulce María Antonio Hernández <al221810679@gmail.com>
2022-01-31 22:58:25 -0600

    login

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg"><br>
+                <center>
+                    <img src="https://images.pexels.com/photos/211122/pexels-photo-211122.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" width="590px"><br>
+                </center>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
