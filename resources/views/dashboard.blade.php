4d50fdcd6259fe6681e0a03f7bb93895580ef7cd
Dulce María Antonio Hernández <al221810679@gmail.com>
2022-01-31 23:06:39 -0600

    A1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg"><br>
                 <center>
+                    Proximamente...
                     <img src="https://images.pexels.com/photos/211122/pexels-photo-211122.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" width="590px"><br>
                 </center>
             </div>
