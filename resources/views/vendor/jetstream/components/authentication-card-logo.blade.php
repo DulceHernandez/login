ff1bd99b95c4c3a1018abdb97188e48f2ebf2d02
Dulce María Antonio Hernández <al221810679@gmail.com>
2022-02-03 09:57:28 -0600

    logo C

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="https://media-exp1.licdn.com/dms/image/C560BAQEwVkFdDq_dag/company-logo_200_200/0/1519868888375?e=2159024400&v=beta&t=JNvrx1vRemXB1TLFVQJ4gp9IRGv8dAzf46yEFDR7P8U" alt="" width="150px">
+<img src="https://fairbanksmorse.mx/img/LogoFMmr-min.png" alt="" width="170px">
